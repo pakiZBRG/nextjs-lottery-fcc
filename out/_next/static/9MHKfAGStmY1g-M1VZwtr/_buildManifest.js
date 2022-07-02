1db1e21b74ebb0a809fb202b9f32de13ee0c985a
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2022-07-02 18:57:15 +0200

    fix gitignore

---
+++ out/_next/static/9MHKfAGStmY1g-M1VZwtr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/39f4d8aba4b66723.css","static/chunks/pages/index-8a064c319bfe81ad.js"],"/_error":["static/chunks/pages/_error-d419484d69bbcf53.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ out/_next/static/chunks/311.02c2c322f0a94b9e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[311],{63018:function(){},27790:function(){},50522:function(){},66882:function(){},11734:function(){},67647:function(){},55896:function(){},87500:function(){}}]);
